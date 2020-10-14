26d0cf9abeec26df1e2e0d02e32884ced7b9e5dc
Alexander Binaei <alex.binaei@carpe.io>
2020-10-13 22:39:47 -0700

    Centered Landing Page

--- src/view/tabs/LandingPage.elm
+++ src/view/tabs/LandingPage.elm
@@ -3,31 +3,28 @@ module View.Tabs.LandingPage exposing (viewLandingPage)
 import Element exposing (Element)
 import Element.Font as Font
 import Msg exposing (Msg)
-import View.Background exposing (background)
-import View.Tabs.Tabs exposing (tabs)
+import View.Background exposing (backgroundWithTabs)
 
 
 viewLandingPage : Element Msg
 viewLandingPage =
-    Element.column
-        background
-        [ tabs
-        , Element.row
-            [ Element.centerY
+    backgroundWithTabs
+        [ Element.column
+            [ Element.centerX
+            , Element.centerY
             , Font.family [ Font.serif ]
+            , Element.spacing 10
             ]
-            [ image1
-            , Element.el
-                [ Element.width (Element.fillPortion 1)
-                ]
-                Element.none
-            , Element.el
+            [ Element.el
                 [ Element.width (Element.fillPortion 1)
                 , Element.alignRight
                 , Font.size 50
                 , Font.letterSpacing 10
                 ]
                 (Element.text "原来如此 Agora Records")
+            , Element.el [] Element.none
+            , Element.el [] Element.none
+            , image1
             ]
         ]
 
@@ -35,8 +32,7 @@ viewLandingPage =
 image1 : Element Msg
 image1 =
     Element.image
-        [ Element.width (Element.fillPortion 10)
-        ]
+        []
         { src = "https://s3-us-west-2.amazonaws.com/images.thetapin.net/landing-page/000022640020.jpeg"
         , description = "image1"
         }
